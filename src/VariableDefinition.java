b1fe6539be79876a4f15867f0ffaac2c4b8d6cea
SHERYL <spennock@missouriwestern.edu>
2023-10-23 14:37:10 -0500

    Initial commit

---
+++ src/VariableDefinition.java
@@ -0,0 +1,18 @@
+public class VariableDefinition {
+
+
+    public static int[][] initializeArrayA() {
+        int[][] arrayA = {{1, 2}, {3, 4}};
+        return arrayA;
+    }
+
+    public static int[][] initializeArrayB() {
+        int[][] arrayB = {{6, 6}, {8, 8}};
+        return arrayB;
+    }
+
+    public static int[][] initializeArrayC() {
+        int[][] arrayC = {{1,2}, {2,1}};
+        return arrayC;
+    }
+}
